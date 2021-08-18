20cb39af9149c5a1b95799972c555ced63a93308
jampool <jampoolgarcia@gmail.com>
2021-08-18 18:28:46 -0400

    proyect structurect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
+// core modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+// external modules
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material/material.module';
+
+// root component
 import { AppComponent } from './app.component';
+
+
+// components
 
 @NgModule({
   declarations: [
-    AppComponent
+    // root
+    AppComponent,
+    // components
   ],
   imports: [
+    // core
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    // routing
+    AppRoutingModule,
+    // ngrx module and cofig
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({}),
+    EffectsModule.forRoot(),
+    // external modules
+    FlexLayoutModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
